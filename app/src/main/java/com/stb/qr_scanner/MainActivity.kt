2107f541dde2e33e064ac4cbd3f519c0ef9a4c87
stb <steffen.bae.sb@gmail.com>
2025-02-27 21:19:39 +0100

    basic scanning functionality

--- app/src/main/java/com/stb/qr_scanner/MainActivity.kt
+++ app/src/main/java/com/stb/qr_scanner/MainActivity.kt
@@ -1,20 +1,73 @@
 package com.stb.qr_scanner
 
+
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.stb.qr_scanner.databinding.ActivityMainBinding
+import android.content.Intent
+import android.widget.Toast
+import com.google.zxing.integration.android.IntentIntegrator
+import org.json.JSONException
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private var qrScanIntegrator: IntentIntegrator? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        setOnClickListener()
+        setupScanner()
+    }
+
+    private fun setupScanner() {
+        qrScanIntegrator = IntentIntegrator(this)
+        qrScanIntegrator?.setOrientationLocked(false)
+    }
+
+    private fun setOnClickListener() {
+        binding.btnScan.setOnClickListener { performAction() }
+
+        binding.showQRScanner.setOnClickListener {
+            // Add code to show QR Scanner Code in Fragment.
+        }
+    }
+
+    private fun performAction() {
+        // Code to perform action when button is clicked.
+        qrScanIntegrator?.initiateScan()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+        if (result != null) {
+            // If QRCode has no data.
+            if (result.contents == null) {
+                Toast.makeText(this, getString(R.string.result_not_found), Toast.LENGTH_LONG).show()
+            } else {
+                // If QRCode contains data.
+                try {
+                    // Converting the data to json format
+                    val obj = JSONObject(result.contents)
+
+                    // Show values in UI.
+                    binding.name.text = obj.getString("name")
+                    binding.siteName.text = obj.getString("site_name")
+
+                } catch (e: JSONException) {
+                    e.printStackTrace()
+
+                    // Data not in the expected format. So, whole object as toast message.
+                    Toast.makeText(this, result.contents, Toast.LENGTH_LONG).show()
+                }
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
         }
     }
 }
