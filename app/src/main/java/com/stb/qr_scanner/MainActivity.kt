34f73edcb6c1d49554a5dae2656669fda9f552c4
stb <steffen.bae.sb@gmail.com>
2025-02-28 14:45:51 +0100

    scanner on startup copy url button open website button link und url display text added

--- app/src/main/java/com/stb/qr_scanner/MainActivity.kt
+++ app/src/main/java/com/stb/qr_scanner/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.stb.qr_scanner
 
 
+import android.content.ClipData
+import android.content.ClipboardManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.stb.qr_scanner.databinding.ActivityMainBinding
 import android.content.Intent
+import android.net.Uri
+import android.text.util.Linkify
+import android.webkit.URLUtil
 import android.widget.Toast
 import com.google.zxing.integration.android.IntentIntegrator
 import org.json.JSONException
@@ -21,25 +26,64 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
-
-        setOnClickListener()
+        //link buttons
+        setOnCLickListener()
         setupScanner()
+        //do scann stuff
+        performAction()
     }
 
     private fun setupScanner() {
         qrScanIntegrator = IntentIntegrator(this)
         qrScanIntegrator?.setOrientationLocked(false)
+        qrScanIntegrator?.setBeepEnabled(false)
     }
 
-    private fun setOnClickListener() {
-        binding.btnScan.setOnClickListener { performAction() }
-        binding.showQRScanner.setOnClickListener {
-            // Add code to show QR Scanner Code in Fragment.
+    private fun setOnCLickListener(){
+        binding.buttonCopyLink.setOnClickListener{copyLinktoClipboard()}
+        binding.buttonOpenWebsite.setOnClickListener{openURL()}
+        binding.buttonOpenWebsite.setOnClickListener{newScan()}
+    }
+
+    /**
+     * maybe unnecessary but maybe need to do something before new scanning
+     */
+    private fun newScan(){
+        performAction()
+    }
+
+    /**
+     * opens url that is in 'siteName' text field
+     */
+    private fun openURL(){
+        val url = binding.siteName.text.toString()
+        if (url.isNotEmpty()){
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+            startActivity(intent)
+        }else{
+            Toast.makeText(this,"nothing to open", Toast.LENGTH_SHORT).show()
         }
     }
 
+    /**
+     * copies url to clipboard of phone
+     */
+    private fun copyLinktoClipboard(){
+        val url = binding.siteName.text.toString()
+        if (url.isNotEmpty()) {
+            val clipboard = getSystemService(CLIPBOARD_SERVICE) as ClipboardManager
+            val clip = ClipData.newPlainText("Copied Link", url)
+            clipboard.setPrimaryClip(clip)
+            Toast.makeText(this, "link copied", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, "nothing to copy", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    /**
+     * starts the scanner
+     */
     private fun performAction() {
-        // Code to perform action when button is clicked.
         qrScanIntegrator?.initiateScan()
     }
 
@@ -57,13 +101,17 @@ class MainActivity : AppCompatActivity() {
 
                     // Show values in UI.
                     binding.name.text = obj.getString("name")
+                    binding.siteName.apply {
+                        setAutoLinkMask(Linkify.WEB_URLS)
+                        isClickable = true
+                        isFocusable = true
+                    }
                     binding.siteName.text = obj.getString("site_name")
 
                 } catch (e: JSONException) {
-                    e.printStackTrace()
-
-                    // Data not in the expected format. So, whole object as toast message.
-                    Toast.makeText(this, result.contents, Toast.LENGTH_LONG).show()
+                    //not in json format so just showing url
+                    binding.name.text = "Scanned Data:"
+                    binding.siteName.text = result.contents
                 }
             }
         } else {
